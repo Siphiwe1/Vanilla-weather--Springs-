3893c11e975f243e04385cdbe24886f60a041d36
Siphiwe1 <159583745+Siphiwe1@users.noreply.github.com>
2024-08-27 11:14:01 +0200

    added functions and elements on index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+function refreshWeather(response){
+   let temperatureElement= document.querySelector("#temperature");
+   let temperature= response.data.temperature.current;
+   let cityElement= document.querySelector("#city");
+   let descriptionElement= document.querySelector("#description");
+   let humidityElement= document.querySelector("#humidity");
+   let windSpeedElement- document.querySelector("#wind-speed");
+   let timeElement=document.querySelector("#time");
+   let date= newDate(response.data.time*1000);
+   let iconElement= document.querySelector("#icon");
+   
+   cityElement.innerHTML=response.data.city;
+   timeElement.innerHTML=formatDate(date);
+   descriptionElement.innerHTML= response.data.condition.description;
+   humidityElement.innerHTML=`${response.data.temperature.humidity}%`
+   windSpeedElement.innerHTML= `${response.data.wind-windSpeed}KM/hr`
+   temperatureElement.innerHTML= math.round(temperature);
+   iconElement.innerHTML= `img src="${response.data.condition.icon-url}"cl`
+}
